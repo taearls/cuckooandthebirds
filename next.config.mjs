fecb4f935f9a5d1e153bd70e1ed2d73debb0e3cb
Tyler Earls <tyler.a.earls@gmail.com>
2024-11-16 16:25:27 -0600

    initial commit, copying portfolio to start

---
+++ next.config.mjs
@@ -0,0 +1,21 @@
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/taearls/**",
+      },
+    ],
+  },
+};
+
+if (process.env.NODE_ENV === "development") {
+  await setupDevPlatform();
+}
+
+export default nextConfig;
