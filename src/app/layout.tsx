d5c4125e532d7a58aecf69af4b31f98ce3521660
Tyler Earls <tyler.a.earls@gmail.com>
2024-11-19 15:50:08 -0600

    update open graph for website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,15 @@ import { Header, Footer, PageContainer, Body } from "@/components/layout";
 
 export const metadata: Metadata = {
   openGraph: {
-    title: "Cuckoo and the Birds",
-    description: "Cuckoo and the Birds is a rock band based in Chicago, IL.",
+    title: "Cuckoo and the Birds | Chicago, IL",
+    description:
+      "Cuckoo and the Birds is a rock band based in Chicago, IL who write sad and angry songs that flirt with harmonies.",
     type: "website",
     url: "https://www.cuckooandthebirds.com",
-    // images: {
-    //   url: "https://res.cloudinary.com/taearls/image/upload/c_scale,w_450/v1605418591/teal-tyler-in-space.jpg",
-    //   alt: "Teal Tyler in Space",
-    // },
+    images: {
+      url: "https://res.cloudinary.com/cuckooandthebirds/image/upload/c_scale,w_450/v1590178958/Twin%20Stars/laser-tyler-og.png",
+      alt: "Laser Tyler",
+    },
   },
   authors: {
     name: "Tyler Earls",
@@ -19,8 +20,7 @@ export const metadata: Metadata = {
   title: "Cuckoo and the Birds",
   description: "Cuckoo and the Birds is a rock band based in Chicago, IL.",
   keywords:
-    "Chicago, Music, Indie, Rock, Pop, Alternative, Midwest, Guitar, Bass, Drums",
-  // "HTML, CSS, JavaScript, Java, Rust, React, Vue, Node.js, Software Engineer, Web Development, SASS, SCSS, Less, Responsive Design, Software Engineer, Frontend, Fullstack",
+    "Chicago, Illinois, DIY, Music, Punk, Indie, Rock, Alt Country, Pop, Alternative, Midwest, Guitar, Bass, Drums, Vocals, Live Music, Cuckoo, Birds, Cuckoo and the Birds",
 };
 
 export default function RootLayout({
