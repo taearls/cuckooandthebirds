61e213af6049206f4e9e54a9654da3b2789f7461
Tyler Earls <tyler.a.earls@gmail.com>
2024-12-24 13:49:41 -0600

    wip

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 // import { type RouteConfig } from "@react-router/dev/routes";
+// https://reactrouter.com/upgrading/component-routes
 import { flatRoutes } from "@react-router/fs-routes";
 
 export default flatRoutes({
