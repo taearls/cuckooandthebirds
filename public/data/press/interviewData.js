a30abf82ad443e883c83c5d10f2a7099425434c3
Tyler Earls <tyler.a.earls@gmail.com>
2024-12-26 12:05:17 -0600

    finish adding stuff from legacy site

---
+++ public/data/press/interviewData.js
@@ -0,0 +1,7 @@
+// TODO: add type definition for this
+export const interviews = [
+  {
+    text: "Kill the Music (September 2019)",
+    href: "https://www.killthemusic.net/blog/unsigned-spotlight-cuckoo-and-the-birds",
+  },
+];
