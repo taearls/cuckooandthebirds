0cc3c4da9c11d608bae74b0f77b662cde921dd85
Tyler Earls <tyler.a.earls@gmail.com>
2025-01-04 12:32:45 -0600

    style updates

--- src/components/layout/headings/HeadingOne.tsx
+++ src/components/layout/headings/HeadingOne.tsx
@@ -1,4 +1,5 @@
 import { TextAlignment } from "@/types/layout";
+import { mergeClasses } from "@/util/styling/styling.utils";
 import { getTextAlignmentClass } from "@/util/utils";
 
 import { HeadingProps } from "./types";
@@ -6,10 +7,17 @@ import { HeadingProps } from "./types";
 export default function HeadingOne({
   children,
   align = TextAlignment.CENTER,
+  accent = true,
 }: HeadingProps) {
   const alignmentClass = getTextAlignmentClass(align);
   return (
-    <h1 className={`${alignmentClass} mb-4 text-4xl font-extrabold`}>
+    <h1
+      className={mergeClasses(
+        alignmentClass,
+        accent && "accent",
+        "mb-4 text-4xl font-extrabold",
+      )}
+    >
       {children}
     </h1>
   );
--- src/components/layout/headings/HeadingTwo.tsx
+++ src/components/layout/headings/HeadingTwo.tsx
@@ -1,4 +1,5 @@
 import { TextAlignment } from "@/types/layout";
+import { mergeClasses } from "@/util/styling/styling.utils";
 import { getTextAlignmentClass } from "@/util/utils";
 
 import { HeadingProps } from "./types";
@@ -6,11 +7,18 @@ import { HeadingProps } from "./types";
 export default function HeadingTwo({
   children,
   align = TextAlignment.CENTER,
+  accent = true,
 }: HeadingProps) {
   const alignmentClass = getTextAlignmentClass(align);
 
   return (
-    <h2 className={`${alignmentClass} accent mb-8 text-3xl font-bold `}>
+    <h2
+      className={mergeClasses(
+        alignmentClass,
+        accent && "accent",
+        "mb-8 text-3xl font-bold",
+      )}
+    >
       {children}
     </h2>
   );
