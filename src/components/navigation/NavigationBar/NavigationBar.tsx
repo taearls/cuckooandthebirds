d613c36be7422a8e146a34c2a2a63551112ee1b2
Tyler Earls <tyler.a.earls@gmail.com>
2025-01-04 10:43:16 -0600

    fix display of nav

--- src/components/navigation/NavigationBar/NavigationBar.tsx
+++ src/components/navigation/NavigationBar/NavigationBar.tsx
@@ -6,6 +6,7 @@ import { useState } from "react";
 import { NavLink } from "react-router";
 
 import NavigationToggle from "../NavigationToggle/NavigationToggle";
+import styles from "./NavigationBar.module.css";
 import NavigationBarListItem from "./NavigationBarListItem";
 
 export type NavigationBarProps = {
@@ -16,55 +17,50 @@ export default function NavigationBar({ links }: NavigationBarProps) {
   const [active, setActive] = useState<boolean>(false);
 
   return (
-    <div className="h-fit w-full">
-      <nav
+    // <div className="h-fit w-full">
+    <nav className={mergeClasses(styles["navigation-bar"])}>
+      <ul
+        role="menu"
         className={mergeClasses(
-          active && "",
-          !active && "justify-end",
-          "fixed top-0 flex min-h-16 w-full border border-b border-l-0 border-r-0 border-t-0",
+          // !active && "hidden",
+          active &&
+            "sm:h-16 sm:flex-row flex-col justify-self-center self-center",
+          styles["navigation-list-container"],
+          "inline-flex h-auto w-40 items-center justify-center sm:w-full",
         )}
       >
-        <ul
-          role="menu"
-          className={mergeClasses(
-            active && "sm:h-16 sm:flex-row flex-col self-center",
-            "inline-flex h-auto w-40 items-center justify-center sm:w-full",
-          )}
-        >
-          <RenderIf condition={active}>
-            {links.map((link, index) => {
-              return (
-                <NavigationBarListItem
-                  key={index}
-                  isLast={index === links.length - 1}
-                >
-                  <NavLink to={link.href} aria-label={link.ariaLabel}>
-                    <InlineAnchorContent
-                      isExternal={Boolean(link.isExternal)}
-                      bold
-                      underline={false}
-                    >
-                      {link.name}
-                    </InlineAnchorContent>
-                  </NavLink>
-                </NavigationBarListItem>
-              );
-            })}
-          </RenderIf>
-        </ul>
+        <RenderIf condition={active}>
+          {links.map((link, index) => {
+            return (
+              <NavigationBarListItem
+                key={index}
+                isLast={index === links.length - 1}
+              >
+                <NavLink to={link.href} aria-label={link.ariaLabel}>
+                  <InlineAnchorContent
+                    isExternal={Boolean(link.isExternal)}
+                    bold
+                    underline={false}
+                  >
+                    {link.name}
+                  </InlineAnchorContent>
+                </NavLink>
+              </NavigationBarListItem>
+            );
+          })}
+        </RenderIf>
+      </ul>
 
-        <div
-          className={mergeClasses(
-            !active && "",
-            "inline-flex self-end my-auto mx-4 ml-[calc(-36px-1rem)]",
-          )}
-        >
-          <NavigationToggle
-            active={active}
-            onClick={() => setActive(!active)}
-          />
-        </div>
-      </nav>
-    </div>
+      <div
+        className={mergeClasses(
+          !active && "",
+          styles["navigation-toggle-container"],
+          "inline-block self-start m-4 w-fit ml-[calc(-36px-1rem)]",
+        )}
+      >
+        <NavigationToggle active={active} onClick={() => setActive(!active)} />
+      </div>
+    </nav>
+    // </div>
   );
 }
