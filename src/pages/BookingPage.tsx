11b4489e71f7143d9ea3a6f7b6c4888e90ea8e1c
Tyler Earls <tyler.a.earls@gmail.com>
2025-01-05 13:57:54 -0600

    update css in booking and not found pag

--- src/pages/BookingPage.tsx
+++ src/pages/BookingPage.tsx
@@ -1,4 +1,5 @@
 import InlineAnchor from "@/components/InlineAnchor/InlineAnchor";
+import FlexContainer from "@/components/layout/containers/FlexContainer/FlexContainer";
 import HeadingOne from "@/components/layout/headings/HeadingOne";
 import Paragraph from "@/components/layout/Paragraph/Paragraph";
 import { WEBSITE_EMAIL } from "@/util/constants/constants";
@@ -6,21 +7,23 @@ import { WEBSITE_EMAIL } from "@/util/constants/constants";
 export default function BookingPage() {
   return (
     <main>
-      <HeadingOne>{"Booking"}</HeadingOne>
-      <Paragraph>
-        Cuckoo and the Birds is fully vaxxed and ready to rock!
-      </Paragraph>
-      <Paragraph>
-        {"The best way to contact us is via email at "}
-        <InlineAnchor
-          href={`mailto:${WEBSITE_EMAIL}`}
-          ariaLabel="Send an email to Cuckoo and the Birds"
-          accent
-        >
-          {WEBSITE_EMAIL}
-        </InlineAnchor>
-        {"."}
-      </Paragraph>
+      <FlexContainer flexFlow="column" gapY={4}>
+        <HeadingOne>{"Booking"}</HeadingOne>
+        <Paragraph>
+          Cuckoo and the Birds is fully vaxxed and ready to rock!
+        </Paragraph>
+        <Paragraph>
+          {"The best way to contact us is via email at "}
+          <InlineAnchor
+            href={`mailto:${WEBSITE_EMAIL}`}
+            ariaLabel="Send an email to Cuckoo and the Birds"
+            accent
+          >
+            {WEBSITE_EMAIL}
+          </InlineAnchor>
+          {"."}
+        </Paragraph>
+      </FlexContainer>
       {/* TODO: add Contact Form */}
       {/* <ContactForm /> */}
     </main>
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,3 +1,4 @@
+import FlexContainer from "@/components/layout/containers/FlexContainer/FlexContainer";
 import HeadingOne from "@/components/layout/headings/HeadingOne";
 import Paragraph from "@/components/layout/Paragraph/Paragraph";
 import { useLocation, useNavigate } from "react-router";
@@ -8,22 +9,24 @@ export default function NotFoundPage() {
 
   return (
     <main>
-      <HeadingOne>Oh no!</HeadingOne>
-      <Paragraph>
-        <code>
-          {`${window.location.host}${location.pathname}${location.search}${location.hash}`}
-        </code>{" "}
-        does not exist.
-      </Paragraph>
-      <button
-        className={"accent text-xl underline"}
-        onClick={
-          // Attempt to recover by navigating to previous page
-          () => navigate(-1)
-        }
-      >
-        Return to Previous Page
-      </button>
+      <FlexContainer flexFlow="column" gapY={8}>
+        <HeadingOne>Oh no!</HeadingOne>
+        <Paragraph>
+          <code>
+            {`${window.location.host}${location.pathname}${location.search}${location.hash}`}
+          </code>{" "}
+          does not exist.
+        </Paragraph>
+        <button
+          className={"accent text-xl underline w-fit"}
+          onClick={
+            // Attempt to recover by navigating to previous page
+            () => navigate(-1)
+          }
+        >
+          Return to Previous Page
+        </button>
+      </FlexContainer>
     </main>
   );
 }
