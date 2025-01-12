21a41dba1330e33361974517a884b06b32143f71
Tyler Earls <tyler.a.earls@gmail.com>
2025-01-12 12:28:57 -0600

    remove comments

--- src/components/CloudinaryImage/CloudinaryImage.tsx
+++ src/components/CloudinaryImage/CloudinaryImage.tsx
@@ -1,27 +1,15 @@
 import { CLOUDINARY_INSTANCE } from "@/util/constants/constants";
 import { fill } from "@cloudinary/url-gen/actions/resize";
 
-// import {
-//   accessibility,
-//   AdvancedImage,
-//   lazyload,
-//   placeholder,
-//   responsive,
-// } from "@cloudinary/react";
-
 export type CloudinaryImageProps = {
-  // transformations?: Array<string>;
   alt: string;
-  // extension: string;
   publicId: string;
   width?: number;
   height?: number;
 };
 
 export default function CloudinaryImage({
-  // transformations = [],
   alt,
-  // extension,
   publicId,
   width = 500,
   height = 500,
